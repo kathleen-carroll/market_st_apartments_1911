e68c6aa7fecf160987e2456bc1954dfc3faaa5b3
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-11 09:23:54 -0700

    Finish iteration 1 by creating apartment class that passes tests

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,1 +1,15 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
+  def initialize(apartment_attributes)
+    @number = apartment_attributes[:number]
+    @monthly_rent = apartment_attributes[:monthly_rent]
+    @bathrooms = apartment_attributes[:bathrooms]
+    @bedrooms = apartment_attributes[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+end
