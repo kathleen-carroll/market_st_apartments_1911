0ea732f3b2ae2daa1c1c4bede30c30a6aeb2c8a2
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-11 09:12:13 -0700

    Create renter class and pass tests

---
+++ test/renter_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/renter"
+
+class RenterTest < Minitest::Test
+  def setup
+    @renter1 = Renter.new("Jessie")
+  end
+
+  def test_it_exists
+    assert_instance_of Renter, @renter1
+  end
+
+  def test_it_has_attributes
+    assert_equal "Jessie", @renter1.name
+  end
+end
